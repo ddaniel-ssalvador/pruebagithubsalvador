815c7874fa6042d7854dc64f91adda16daf5244f
ddaniel-ssalvador <168252688+ddaniel-ssalvador@users.noreply.github.com>
2024-04-26 21:38:48 -0600

    Confirmacion Salvador 1
    Ok

---
+++ Sesion23SalvadorISGITHUB/src/sesion23salvadorisgithub/Sesion23SalvadorISGITHUB.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sesion23salvadorisgithub;
+
+/**
+ *
+ * @author ddani
+ */
+public class Sesion23SalvadorISGITHUB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Prueba de GITHUB 1 SALVADOR");
+    }
+    
+}
